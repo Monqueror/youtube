74c4a9f570dbe50fce2fa453efeff0378e7a5e25
高槻やよい <qazwsxedcrfvtgbyhnujmikol@takatsukiyayoi.local>
2017-04-07 05:34:09 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+const http = require("http");
+
+const rooting = require("./src/rooting");
+
+let app = express();
+let server = http.createServer(app);
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+let option = {
+  host: "localhost",
+	port: 3306,
+	user: "root",
+	database: "youtube"
+};
+
+app.use(session({
+  secret: "youtube cat",
+  resave: false,
+  saveUninitialized: false,
+  store: new MySQLStore(option),
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 30
+  }
+}));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+process.on('uncaughtException', function(err) {
+  console.error(err);
+});
+
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
